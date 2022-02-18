0a46fdae8915a33f71322b2fe0a5b5666102ec7b
HernanBueno <40526403+HernanBueno@users.noreply.github.com>
2022-02-18 11:36:20 -0300

    v1.0

--- node/database/db.js
+++ node/database/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-const url = 'mongodb+srv://root:root@cluster0.ixidx.mongodb.net/blog_crud?retryWrites=true&w=majority'
+const url = urldb
 mongoose.connect(url);
 const db = mongoose.connection;
 db.on('open', () => { console.log('conectado a mongodb') });
